1d186b4cb4e77e6fe53f15080afc72814fce9e71
usmanjamil83 <usmanjamil83@hotmail.com>
2017-04-27 12:39:06 -0400

    fix .js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
 // Initial Values
 var name = "";
 var destination = "";
@@ -28,12 +25,30 @@ $("#add-user").on("click", function(event) {
   time = $("#train-time").val().trim();
   frequency = $("#frequency").val().trim();
 
+      //convert first time to determine when the next time will be
+      //subtract a year to work with the times
+      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+      //format the current time 
+      var currentTime = moment().format("HH:mm");
+      //find the differenence in now and the first train time in minutes
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      // find the remainder of the difference in now and first time 
+      var tRemainder = diffTime % frequency;
+      //subtract remainder from the frequency. 
+      var minutesTillTrain = frequency - tRemainder;
+      //add mintuestill next train to the current time in minutes
+      var nextTrain = moment().add(minutesTillTrain, "minutes");
+      //format 
+      var nextTrainFormatted = moment(nextTrain).format("HH:mm");
+
   // Code for "Setting values in the database"
   database.ref().push({
     name: name,
     destination: destination,
     time: time,
     frequency: frequency,
+    nextTrainFormatted: nextTrainFormatted,
+    minutesTillTrain: minutesTillTrain,
     dateAdded : firebase.database.ServerValue.TIMESTAMP
   });
 });
@@ -45,13 +60,13 @@ console.log(childSnapshot.val().name);
 console.log(childSnapshot.val().destination);
 console.log(childSnapshot.val().time);
 console.log(childSnapshot.val().frequency);
-// console.log(childSnapshot.val().monthsworked);
-// console.log(childSnapshot.val().totalpaid);
+console.log(childSnapshot.val().nextTrainFormatted);
+console.log(childSnapshot.val().minutesTillTrain);
 
 // full list of items to the well
 $("#table").append('<tr><th>' + childSnapshot.val().name + '</th><th>' + childSnapshot.val().destination +
-  '</th><th>' + childSnapshot.val().frequency + '</th><th>' + childSnapshot.val().NextArrival + '</th><th>' +
-  childSnapshot.val().MinutesAway + '</th><th>');
+  '</th><th>' + childSnapshot.val().frequency + '</th><th>' + childSnapshot.val().nextTrainFormatted + '</th><th>' +
+  childSnapshot.val().minutesTillTrain + '</th><th>');
 
    // Handle the errors
  }, function(errorObject) {
